742ffe72257ac9b2e548fe31869ac3e7f16e2d4d
Christian Crisologo <Christian.Crisologo@gecogaming.com>
2017-03-14 14:51:00 +1100

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'WOW ANGULAR 4';
+  angularVersion = 4;
+  isAngular4 = this.angularVersion ===4;
 }
